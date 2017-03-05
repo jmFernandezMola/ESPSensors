f4b11d4d3119f65728a222ebd93196496cdd38c0
bitllenc <gframon@gmail.com>
2017-03-05 23:47:23 +0100

    S'han afetgit controls d'excepcions al parsejar i manipular l'XML amb la llibreria Untangle

--- pyEnviR_MQTT.py
+++ pyEnviR_MQTT.py
@@ -6,36 +6,45 @@ import serial
 
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+	print("Connected with result code "+str(rc))
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    #client.subscribe("$SYS/#")
+	# Subscribing in on_connect() means that if we lose the connection and
+	# reconnect then subscriptions will be renewed.
+	#client.subscribe("$SYS/#")
 
-# The callback for when a PUBLISH message is received from the server.
+	# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    
+	print(msg.topic+" "+str(msg.payload))
+
 def getCCdata(port='/dev/ttyAMA0', verbose=False):
 	"""Gets temperature and power usage data from an attached CurrentCost meter.
 
 	Parameters:
-	 - port: the port that the CurrentCost meter is attached to. Somthing like /dev/ttyUSB0 or COM1
+	 port: the port that the CurrentCost meter is attached to. Somthing like /dev/ttyUSB0 or COM1
 
-	Returns:
-	(temperature, usage), with temperature in degrees C, and usage in Watts
-	"""
+    Returns:
+    (temperature, usage), with temperature in degrees C, and usage in Watts
+    """
 	ser = serial.Serial(port, 57600)
 	xmldata = ser.readline()
 	if verbose:
 		print(xmldata)
 	ser.close()
-	p = untangle.parse(xmldata.decode("utf-8"))
-	temperature = float(p.msg.tmpr.cdata)
-	watts = int(p.msg.ch1.watts.cdata)
-	print(watts)
-	return (watts, temperature)
-
+	
+	try :
+		p = untangle.parse(xmldata.decode("utf-8"))
+		temperature = float(p.msg.tmpr.cdata)
+		watts = int(p.msg.ch1.watts.cdata)
+	except IndexError:
+		print("Error parsing XML, maybe a history packet: IndexError")
+		temperature = None
+		watts = None
+	except :
+		print("Error with the XML from CurrentCost: Exception")
+		temperature = None
+		watts = None
+	finally:
+		return (watts, temperature)
 
 
 mqttClient = mqtt.Client(client_id="python_client", clean_session=True, userdata=None, protocol="MQTTv311")
@@ -49,19 +58,19 @@ counter = 0
 
 #mqttClient.loop_start()
 while True:
-	counter = counter + 1
-	mqttClient.publish("home/py", payload=counter, qos=0, retain=False)
-	mqttClient.subscribe("home/temp", 0)
-	(CCpow, CCtemp) = getCCdata(port='/dev/ttyAMA0',verbose=True)
-	#print('Temp: ', CCtemp,'- Power: ', CCpow)
-	mqttClient.publish("home/CC/power", payload=CCpow, qos=0, retain=False)
-	mqttClient.publish("home/CC/temp", payload=CCtemp, qos=0, retain=False)
-	time.sleep(2)
-	mqttClient.loop()
-    
+    counter = counter + 1
+    mqttClient.publish("home/py", payload=counter, qos=0, retain=False)
+    mqttClient.subscribe("home/temp", 0)
+    (CCpow, CCtemp) = getCCdata(port='/dev/ttyAMA0',verbose=True)
+    #print('Temp: ', CCtemp,'- Power: ', CCpow)
+    mqttClient.publish("home/CC/power", payload=CCpow, qos=0, retain=False)
+    mqttClient.publish("home/CC/temp", payload=CCtemp, qos=0, retain=False)
+    time.sleep(2)
+    mqttClient.loop()
+
 #mqttClient.loop_forever()
 
 
 
-    
+
 
