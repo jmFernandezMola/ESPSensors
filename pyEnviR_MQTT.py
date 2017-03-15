4099de7a2d39fb37ee44f40fb8ba5fb1ac5e4d95
bitllenc <gframon@gmail.com>
2017-03-15 01:04:43 +0100

    Afegit el control d'excepcions en l'adquisició de dades pel port sèrie i el parsejat de l'XML

--- pyEnviR_MQTT.py
+++ pyEnviR_MQTT.py
@@ -16,36 +16,37 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
 	print(msg.topic+" "+str(msg.payload))
 
-def getCCdata(port='/dev/ttyAMA0', verbose=False):
-	"""Gets temperature and power usage data from an attached CurrentCost meter.
-
-	Parameters:
-	 port: the port that the CurrentCost meter is attached to. Somthing like /dev/ttyUSB0 or COM1
-
-    Returns:
-    (temperature, usage), with temperature in degrees C, and usage in Watts
-    """
-	ser = serial.Serial(port, 57600)
-	xmldata = ser.readline()
-	if verbose:
-		print(xmldata)
-	ser.close()
+def getCCdata(port, verbose=False):
+#	ser = serial.Serial(port, 57600)
+	#if not port.is_open():
+	#	port.open()
 	
 	try :
+		port.open()
+		xmldata = port.readline()
+
+		if verbose:
+			print(xmldata)
+			
+		port.close()
+		
 		p = untangle.parse(xmldata.decode("utf-8"))
 		temperature = float(p.msg.tmpr.cdata)
 		watts = int(p.msg.ch1.watts.cdata)
+		print("P: ", watts, "W; ", "T: ", temperature, "ºC")
 	except IndexError:
 		print("Error parsing XML, maybe a history packet: IndexError")
 		temperature = None
 		watts = None
 	except :
-		print("Error with the XML from CurrentCost: Exception")
+		print("Exception: General")
 		temperature = None
 		watts = None
 	finally:
 		return (watts, temperature)
 
+
+ser = serial.Serial(port='/dev/ttyAMA0', baudrate=57600, timeout=10)
 
 mqttClient = mqtt.Client(client_id="python_client", clean_session=True, userdata=None, protocol="MQTTv311")
 
@@ -58,15 +59,15 @@ counter = 0
 
 #mqttClient.loop_start()
 while True:
-    counter = counter + 1
-    mqttClient.publish("home/py", payload=counter, qos=0, retain=False)
-    mqttClient.subscribe("home/temp", 0)
-    (CCpow, CCtemp) = getCCdata(port='/dev/ttyAMA0',verbose=True)
-    #print('Temp: ', CCtemp,'- Power: ', CCpow)
-    mqttClient.publish("home/CC/power", payload=CCpow, qos=0, retain=False)
-    mqttClient.publish("home/CC/temp", payload=CCtemp, qos=0, retain=False)
-    time.sleep(2)
-    mqttClient.loop()
+	counter = counter + 1
+	mqttClient.publish("home/py", payload=counter, qos=0, retain=False)
+	mqttClient.subscribe("home/temp", 0)
+	(CCpow, CCtemp) = getCCdata(port=ser,verbose=False)
+	#print('Temp: ', CCtemp,'- Power: ', CCpow)
+	mqttClient.publish("home/CC/power", payload=CCpow, qos=0, retain=False)
+	mqttClient.publish("home/CC/temp", payload=CCtemp, qos=0, retain=False)
+	time.sleep(2)
+	mqttClient.loop()
 
 #mqttClient.loop_forever()
 
