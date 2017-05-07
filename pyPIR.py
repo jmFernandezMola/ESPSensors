31dba7b2c31f64d0dc946dda388fa2a0f3c44738
bitllenc <gframon@gmail.com>
2017-05-08 01:29:48 +0200

    Canviat els valors d'actiu i inactiu del sensor PIR enviats per MQTT (per millorar la interfície d'MQTT Dashboard)

--- pyPIR.py
+++ pyPIR.py
@@ -12,7 +12,7 @@ led = LED(17)
 def on_move():
     led.blink(on_time = 0.5, off_time = 0.5, n = 1, background = True)
     print(time.asctime(time.localtime()) + " - YOU MOVED!")
-    mqttClient.publish("home/py/motion", payload="Motion", qos=0, retain=False)
+    mqttClient.publish("home/py/motion", payload="1", qos=0, retain=False)
 
 
 mqttClient = mqtt.Client(client_id="pyPIR_sensor", clean_session=True, userdata=None, protocol="MQTTv311")
@@ -27,7 +27,7 @@ while True:
         #led.blink(on_time = 0.5, off_time = 0.5, n = 1, background = True)
         #print("You moved!!")
     #pir.wait_for_no_motion()
-    mqttClient.publish("home/py/motion", payload="Quiet", qos=0, retain=False)
+    mqttClient.publish("home/py/motion", payload="0", qos=0, retain=False)
     time.sleep(5)
     #pir.wait_for_motion()
     #led.toggle()
