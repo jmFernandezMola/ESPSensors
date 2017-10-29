5c984133b8f969a1fa7c1e1c241da2555ec87aeb
bitllenc <gframon@gmail.com>
2017-10-30 00:21:29 +0100

    Lectures de l'EnviR correctes Integració lectures EnviR a INfluxDB
    S'afegeix la gestió de logs i s'eliminen els print
    S'arrenquen els scripts de python des de crontab -> pyBoot.sh

--- pyEnviR_MQTT.py
+++ pyEnviR_MQTT.py
@@ -3,10 +3,13 @@ import paho.mqtt.client as mqtt
 import time
 import untangle #per processar XMLs
 import serial
-from gpiozero import LED, Button
+import logging
+import logging.handlers
+import sys
+#from gpiozero import LED, Button
 
 def on_connect(client, userdata, flags, rc):
-	print("Connected with result code "+str(rc))
+	log.debug("Connected with result code "+str(rc))
 
 	# Subscribing in on_connect() means that if we lose the connection and
 	# reconnect then subscriptions will be renewed.
@@ -14,62 +17,85 @@ def on_connect(client, userdata, flags, rc):
 
 	# The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-	print(msg.topic+" "+str(msg.payload))
+	log.debug(msg.topic+" "+str(msg.payload))
 
+	
 def getCCdata(port, verbose=False):
 #	ser = serial.Serial(port, 57600)
 	#if not port.is_open():
 	#	port.open()
 	
 	try :
-		port.open()
-		xmldata = port.readline()
-
-		if verbose:
-			print(xmldata)
-			
-		port.close()
+		if (serialPort == None) :
+			log.debug("Serial port found closed!. Opening...")
+			serialPort.open()
+		
+		xmldata = serialPort.readline()
+		
+		log.debug(xmldata)
 		
 		p = untangle.parse(xmldata.decode("utf-8"))
+		
+		#Getting data from the received xml stream
 		temperature = float(p.msg.tmpr.cdata)
 		watts = int(p.msg.ch1.watts.cdata)
-		print("P: ", watts, "W; ", "T: ", temperature, "ºC")
+		#print("P: ", watts, "W; ", "T: ", temperature, "ºC")
+		log.info('Power: %s W; Temp: %s C', watts, temperature)
+
+
 	except IndexError:
-		print("Error parsing XML, maybe a history packet: IndexError")
+		log.exception("Error parsing XML, maybe a history packet: IndexError")
 		temperature = None
 		watts = None
 	except :
-		print("Exception: General")
+		log.exception('Reading data: %s', sys.exc_info()[1])
+		#print("Exception: General")
 		temperature = None
 		watts = None
 	finally:
 		return (watts, temperature)
 
 
-ser = serial.Serial(port='/dev/ttyAMA0', baudrate=57600, timeout=10)
+urlRaspi_local = "localhost"
+urlRaspi_remot = "http://elponipisador.no-ip.org"
+devSerialPort = '/dev/serial0'
 
-mqttClient = mqtt.Client(client_id="python_client", clean_session=True, userdata=None, protocol="MQTTv311")
+#LOGGER setup
+log = logging.getLogger('__name__')
+log.setLevel(logging.INFO)
+logHdlr = logging.handlers.SysLogHandler('/dev/log')
+logFormat = logging.Formatter('EnviR: %(levelname)s - %(message)s')
+logHdlr.setFormatter(logFormat)
+log.addHandler(logHdlr)
 
-mqttClient.on_connect = on_connect
-mqttClient.on_message = on_message
 
-mqttClient.connect("192.168.1.92",port=1883,keepalive=60)
+try :
+	#SERIAL PORT
+	serialPort = serial.Serial(port=devSerialPort, baudrate=57600, timeout=10)
 
-counter = 0
+	#MQTT
+	mqttClient = mqtt.Client() #client_id="python_client", clean_session=True, userdata=None, protocol="MQTTv311")
+	mqttClient.on_connect = on_connect
+	mqttClient.on_message = on_message
+	mqttClient.connect(urlRaspi_local,port=1883,keepalive=60)
+	
+except :
+	log.exception('Opening connections: %s', sys.exc_info()[1])
 
-#mqttClient.loop_start()
+
+
 while True:
-	counter = counter + 1
-	mqttClient.publish("home/py", payload=counter, qos=0, retain=False)
-	mqttClient.subscribe("home/temp", 0)
-	(CCpow, CCtemp) = getCCdata(port=ser,verbose=False)
-	#print('Temp: ', CCtemp,'- Power: ', CCpow)
-	mqttClient.publish("home/CC/power", payload=CCpow, qos=0, retain=False)
-	mqttClient.publish("home/CC/temp", payload=CCtemp, qos=0, retain=False)
-	time.sleep(2)
+	(CCpow, CCtemp) = getCCdata(port=serialPort,verbose=False)
+	
+	if CCpow != None:
+		mqttClient.publish("home/CC/power", payload=CCpow, qos=0, retain=False)
+	
+	if CCtemp != None:
+		mqttClient.publish("home/CC/temp", payload=CCtemp, qos=0, retain=False)
+	
 	mqttClient.loop()
+	time.sleep(2)
 
-#mqttClient.loop_forever()
 
 
 
--- pyMQTT_Influx.py
+++ pyMQTT_Influx.py
@@ -7,58 +7,57 @@ Created on Sun Oct 22 22:49:17 2017
 
 import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
-from datetime import datetime
-
+#from datetime import datetime
+import logging
+import logging.handlers
+import sys
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(MQTTclient, userdata, flags, rc):
-	print("Connected with result code "+str(rc))
-
+	log.debug("Connected with result code %s", str(rc))
 	# Subscribing in on_connect() means that if we lose the connection and
 	# reconnect then subscriptions will be renewed.
 	MQTTclient.subscribe("home/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(MQTTclient, userdata, msg):
-	print(str(datetime.now()) + " - " + msg.topic+" "+str(msg.payload))
-#	json_body = [
-#		{
-#		"measurement": "measurement",
-#		"tags": {
-#			"type": msg.topic.split('/')[2],
-#			"location": msg.topic.split('/')[1]
-#		},
-#		"fields": {
-#			"value": (float(msg.payload))
-#			}
-#		}
-#	]
-	#print(json_body)
-	#ifClient.write(json_body,protocol='json')
-	#ifClient.write_points(json_body, database='openhab', protocol='json')
+	#print(str(datetime.now()) + " - " + msg.topic+" "+str(msg.payload))
 	
 	line = ['measurement,type=' + msg.topic.split('/')[2] + ',location=' + msg.topic.split('/')[1] + ' value=' + str(float(msg.payload))]
-	#print(line)
+
 	try:
+		log.info("New input: type: %s, location: %s, value: %s", msg.topic.split('/')[2], msg.topic.split('/')[1], str(float(msg.payload)))
 		ifClient.write_points(line, database='openhab', protocol='line')
 	except :
-		print(str(datetime.now()), " - Error sending data to InfluxDB")
+		log.exception("Error sending data to InfluxDB: %s", sys.exc_info()[1])
+		#print(str(datetime.now()), " - Error sending data to InfluxDB")
 
 
 #line = [msg.topic.split('/')[2] + "_test " + float(msg.payload)]
 
+urlRaspi_local = "localhost"
+urlRaspi_remot = "http://elponipisador.no-ip.org"
+devSerialPort = '/dev/serial0'
+
+#LOGGER setup
+log = logging.getLogger('__name__')
+log.setLevel(logging.DEBUG)
+logHdlr = logging.handlers.SysLogHandler('/dev/log')
+logFormat = logging.Formatter('MQTT-Influx: %(levelname)s - %(message)s')
+logHdlr.setFormatter(logFormat)
+log.addHandler(logHdlr)
 
 #MQTT
 MQTTclient = mqtt.Client()
 MQTTclient.on_connect = on_connect
 MQTTclient.on_message = on_message
 
-MQTTclient.connect("192.168.1.92", 1883, 60)
+MQTTclient.connect(urlRaspi_local, 1883, 60)
 
 
 #INFLUX
-ifClient = InfluxDBClient(host='192.168.1.92', port=8086, username='root', password='root', database='openhab')
-print(ifClient.get_list_database())
+ifClient = InfluxDBClient(host=urlRaspi_local, port=8086, username='root', password='root', database='openhab')
+#print(ifClient.get_list_database())
 ifClient.switch_database('openhab')
 
 # Blocking call that processes network traffic, dispatches callbacks and
